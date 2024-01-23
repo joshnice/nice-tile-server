4a9c05336dd664912a4ac91f00ef5d03ed242214
joshnice <joshliamnice@gmail.com>
2024-01-23 19:46:43 +0000

    Updated sql init script

--- database/init.sql
+++ database/init.sql
@@ -1,5 +1,16 @@
--- init.sql
-CREATE TABLE example (
-    id serial PRIMARY KEY,
-    name VARCHAR (100) NOT NULL
+-- Install postgis
+create extension postgis;
+
+-- Maps table
+create table maps (
+    id uuid primary key,
+    name varchar(50)
+);
+
+-- Objects table
+CREATE TABLE objects (
+    id uuid primary key,
+    geom geometry,
+    map_id uuid,
+    constraint fk_map_id foreign key(map_id) references maps(id)
 );
