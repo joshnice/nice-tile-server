ccc561b018eb65949e5950877d852ef40757279f
joshnice <joshliamnice@gmail.com>
2024-03-28 09:34:06 +0000

    Allow for modal to have variable size

--- frontend/src/components/modal.tsx
+++ frontend/src/components/modal.tsx
@@ -1,9 +1,12 @@
-import type { PropsWithChildren } from "react";
+import { useMemo, type PropsWithChildren } from "react";
 import { createPortal } from "react-dom";
 import { HeaderText } from "./basic/headers";
 import { ButtonComponent } from "./basic/buttons";
 
+type Size = "small" | "medium" | "large";
+
 interface ModalProps {
+	size?: Size;
 	header: string;
 	submitButtonText: string;
 	open: boolean;
@@ -12,6 +15,7 @@ interface ModalProps {
 }
 
 export default function ModalComponent({
+	size = "medium",
 	header,
 	submitButtonText,
 	open,
@@ -19,14 +23,15 @@ export default function ModalComponent({
 	onSubmit,
 	children,
 }: PropsWithChildren<ModalProps>) {
+	const modalSizeClasses = useMemo(() => getModalSizeClasses(size), [size]);
 	const portal = createPortal(
 		<dialog
-			className="flex flex-col gap-8 absolute top-[25vh] max-h-[750px] w-[50vw] max-w-[600px] border-2 border-black z-50 p-5"
+			className={`flex flex-col gap-8 absolute border-2 border-black z-50 p-5 ${modalSizeClasses}`}
 			open={open}
 		>
 			<HeaderText title={header} />
 			{children}
-			<div className="modal-submit-button">
+			<div className="w-full flex justify-end gap-5">
 				<ButtonComponent text="Close" onClick={onClose} />
 				<ButtonComponent text={submitButtonText} onClick={onSubmit} />
 			</div>
@@ -49,4 +54,17 @@ export default function ModalComponent({
 			)}
 		</>
 	);
+}
+
+function getModalSizeClasses(size: Size): string {
+	switch (size) {
+		case "small":
+			throw new Error("Size not added");
+		case "medium":
+			return "top-[25vh] max-h-[750px] w-[50vw] max-w-[600px]";
+		case "large":
+			return "top-[10vh] h-[80vh] w-[60vw] max-w-[1000px]";
+		default:
+			throw new Error("Size value not handled");
+	}
 }
