e055cb6f82f2ca273a00fe6a915cc6437e9a1b42
joshnice <joshliamnice@gmail.com>
2024-05-22 22:08:33 +0100

    Update route

--- api/src/local-file-system/read-map-tiles.ts
+++ api/src/local-file-system/read-map-tiles.ts
@@ -3,6 +3,6 @@ import { readFile } from "./local-file-system-read-helpers";
 const basePath = "/home/joshnice/Documents/mapbox-tiles"
 
 export async function getLocalMapTile(x: number, y: number, z: number) {
-    const file = await readFile(`${basePath}/tiles-v2/${z}/${x}/${y}.pbf`, false);
+    const file = await readFile(`${basePath}/nice-tile-server/${z}/${x}/${y}.pbf`, false);
     return file;
 }
