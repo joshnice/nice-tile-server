ee1c9a5242a7abad9b999eebaa9e971cfa6dcfa4
joshnice <joshliamnice@gmail.com>
2024-05-27 17:55:24 +0100

    Created a type module

---
+++ types/src/layer-styles.ts
@@ -0,0 +1,18 @@
+export type AllStyles = FillStyle | LineStyle | PointStyle;
+
+export interface FillStyle {
+    colour: string;
+    opacity: number;
+}
+
+export interface LineStyle {
+    colour: string;
+    opacity: number;
+    size: number;
+}
+
+export interface PointStyle {
+    colour: string;
+    opacity: number;
+    size: number;
+}
