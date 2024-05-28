dcfaf1ad797046af07877a1d49ea09ee7c38bf20
joshnice <joshliamnice@gmail.com>
2024-05-28 19:08:19 +0100

    Added more node local file helpers

--- api/src/local-file-system/local-file-system-read-helpers.ts
+++ api/src/local-file-system/local-file-system-read-helpers.ts
@@ -1,4 +1,5 @@
-import { readFile as readFileNode } from 'node:fs/promises';
+import { readFile as readFileNode, mkdir, rmdir } from "node:fs/promises";
+import { existsSync } from "node:fs";
 
 export async function readFile(path: string, throwErrorIfNotFound = true) {
     try {
@@ -8,5 +9,28 @@ export async function readFile(path: string, throwErrorIfNotFound = true) {
         if (throwErrorIfNotFound) {
             console.error(err);
         }
+    }
+}
+
+export function findDirectory(path: string): boolean {
+    return existsSync(path);
+}
+
+export async function deleteDirectory(path: string, throwErrorIfNotFound = true): Promise<void> {
+    try {
+        await rmdir(path);
+    } catch (err) {
+        if (throwErrorIfNotFound) {
+            console.error(err);
+        }
+    }
+}
+
+export async function createDirectory(path: string, folderName: string) {
+    try {
+        const fullPath = `${path}/${folderName}`;
+        await mkdir(fullPath);
+    } catch (err) {
+        console.error(err);
     }
 }
