8c7b089458da37ca24e43b7bf17e9ab4772ca7d8
joshnice <joshliamnice@gmail.com>
2024-06-01 12:37:55 +0100

    Add tile set to db

--- api/src/models/map-tiles.ts
+++ api/src/models/map-tiles.ts
@@ -1,6 +1,8 @@
 import type { Feature, FeatureCollection } from "geojson";
+import { v4 as uuid } from "uuid";
 import { createGeoJSONFile, createMapTilesDirectory, getLocalMapTile, runMbUtil, runTippecanoe } from "../local-file-system/map-tiles";
 import { listObjectsByMapId } from "./objects";
+import { client } from "../db/connection";
 
 export function getMapTile(x: number, y: number, z: number) {
     return getLocalMapTile(x, y, z);
@@ -29,5 +31,25 @@ export async function createMapTiles(mapId: string) {
 
     await runMbUtil(mapId);
 
-    // Add to db
+    const mapTiles = await getMapTiles(mapId);
+
+    if (mapTiles == null) {
+
+        const SQL = `
+            insert into map_tile_set (id, map_id)
+            values ($1, $2);
+        `;
+
+        await client.query(SQL, [uuid(), mapId]);
+    }
+}
+
+export async function getMapTiles(mapId: string) {
+    const SQL = `
+        select * from map_tile_set where map_id = $1
+    `;
+
+    const response = await client.query(SQL, [mapId]);
+
+    return response.rows[0];
 }
