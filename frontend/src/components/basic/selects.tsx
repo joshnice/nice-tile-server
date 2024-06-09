0b75c97d186fd0db317fec635244c683620ceba9
joshnice <joshliamnice@gmail.com>
2024-06-09 16:34:36 +0100

    Added select with groups component

--- frontend/src/components/basic/selects.tsx
+++ frontend/src/components/basic/selects.tsx
@@ -58,3 +58,43 @@ export function SelectObjectComponent<
 		</select>
 	);
 }
+
+export function SelectObjectComponentWithGroups<
+	TValue extends { id: string; name: string },
+>({
+	value,
+	groups,
+	onChange,
+}: {
+	value: TValue;
+	groups: { name: string; options: TValue[] }[];
+	onChange: (value: TValue) => void;
+}) {
+	const handleValueSelected = (id: string) => {
+		const selectedOption = groups.flatMap(g => g.options).find((o) => o.id === id);
+		if (selectedOption == null) {
+			throw new Error("Option doesn't exist");
+		}
+		onChange(selectedOption);
+	};
+
+	return (
+		<select
+			className="h-12 border-2 border-slate-600 p-3 text-lg full bg-white rounded-sm"
+			value={value.id}
+			onChange={(event) => handleValueSelected(event.target.value)}
+		>
+			{groups.map((group) => (
+				<optgroup key={group.name} label={group.name}>
+					{
+						group.options.map((option) => (
+							<option value={option.id} key={option.id}>
+								{option.name}
+							</option>
+						))
+					}
+				</optgroup>
+			))}
+		</select>
+	);
+}
